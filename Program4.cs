8b9663047fa66b82f5121dde98b16722d1eedbd8
Унугярн Даниел <oreol2005@gmail.com>
2022-08-08 18:19:53 +0300

    first commit

---
+++ Program4.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третие число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+   Console.WriteLine("Первое число " + numberA + " больше чем второе: " + numberB);
+}
+else;
+if (numberA < numberB)
+{
+  Console.WriteLine("Второе число " + numberB + " больше чем первое: " + numberA);
+}
+else;
+if (numberA > numberС)
+{
+   Console.WriteLine("Первое число " + numberA + " больше чем третие: " + numberС);
+}
+else;
+if (numberA < numberС)
+{
+  Console.WriteLine("Третие число " + numberС + " больше чем первое: " + numberA);
+}
+else;
+if (numberB > numberС)
+{
+   Console.WriteLine("Второе число  " + numberB + " больше чем третие: " + numberС);
+}
+else;
+if (numberB < numberС)
+{
+  Console.WriteLine("Третье число " + numberС + " больше чем второе: " + numberB);
+}
+else;
