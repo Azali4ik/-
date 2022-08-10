88016072a6e41a4bb661b63e6528171fe62f38b8
Унугярн Даниел <oreol2005@gmail.com>
2022-08-10 20:47:03 +0300

    first commit

---
+++ Program13.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+
+  if(number < 999)
+ {
+   int lastDigit = number % 10;
+   Console.WriteLine(lastDigit);
+ }
+
+  if((number < 99))
+{
+ Console.WriteLine("Третего числа нет");
+}
