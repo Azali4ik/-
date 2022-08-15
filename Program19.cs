eb243dc5a6d1c0279987275d1296a07c0d9d9eed
Унугярн Даниел <oreol2005@gmail.com>
2022-08-15 21:15:56 +0300

    first commit1

--- Program19.cs
+++ Program19.cs
@@ -8,7 +8,7 @@ Console.Write("Введите число: ");
 int d= Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число: ");
 int f= Convert.ToInt32(Console.ReadLine());
-if(a==f || b==d) 
+if(a==f && b==d) 
 {
     Console.WriteLine("Число являеться полиндромом: ");
 }
