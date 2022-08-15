65ad805a4346ffabb1c22c52d2aa252dd9c3e06c
Унугярн Даниел <oreol2005@gmail.com>
2022-08-15 21:57:59 +0300

    first commit1

---
+++ Program23.cs
@@ -0,0 +1,20 @@
+﻿int num  = ReadInt("Введите число: ");
+int i = 1;
+
+while(i <= num)
+{
+    Console.Write(Cube(i++) + ",");
+}
+
+
+int Cube(int a)
+{
+    return a * a * a;
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
