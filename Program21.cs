fa1ffe5468c28552047c83053087097c38409d72
Унугярн Даниел <oreol2005@gmail.com>
2022-08-19 20:45:23 +0300

    first commit1

--- Program21.cs
+++ Program21.cs
@@ -1,21 +1,10 @@
-﻿int x1 = ReadInt("Введите Х точки А: ");
-int y1 = ReadInt("Введите Y точки А: ");
-int z1 = ReadInt("Введите Z точки А: ");
+﻿Console.Write("Введите число: ");
+string numA= Console.ReadLine();
 
-int x2 = ReadInt("Введите Х точки B: ");
-int y2 = ReadInt("Введите Y точки B: ");
-int z2 = ReadInt("Введите Z точки B: ");
-
-
-int lengthX = x2 - x1; 
-int lengthY = y2 - y1;
-int lengthZ = z2 - z1;
-
-double distance = Math.Sqrt(lengthX * lengthX + lengthY * lengthY + lengthZ * lengthZ);
-Console.WriteLine(distance);
-
-int ReadInt(string message)
+int sum = 0;
+for (int i =0; i < numA.Length; i++ )
 {
-    Console.WriteLine(message);
-    return Convert.ToInt32(Console.ReadLine());
+    string temp = Convert.ToString(numA[i]);
+    sum += Convert.ToInt32(temp);
 }
+Console.WriteLine(sum);
