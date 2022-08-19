22553600ae2faab509714e07f8eb87842e033b8d
Унугярн Даниел <oreol2005@gmail.com>
2022-08-19 20:53:18 +0300

    first commit1

---
+++ Program29.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1, 2, 5, 7, 19};
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " "); 
+    
+}
+ Console.WriteLine();
+int[] arr = {6, 1, 33};
+
+for(int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
