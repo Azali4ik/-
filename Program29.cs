d0a03aa7dd4b51b6355331e3bd4c89a02a8a566e
Унугярн Даниел <oreol2005@gmail.com>
2022-08-24 21:28:29 +0300

    first commit1

--- Program29.cs
+++ Program29.cs
@@ -1,14 +1,18 @@
-﻿int[] array = {1, 2, 5, 7, 19};
-
-for(int i = 0; i < array.Length; i++)
+﻿int size = 10;
+int[] numbers = new int[size];
+int count = 0; 
+for (int i = 1; i < numbers.Length; i++)
 {
-    Console.Write(array[i] + " "); 
-    
-}
- Console.WriteLine();
-int[] arr = {6, 1, 33};
+  
+    numbers[i] = new Random().Next(-99, 100);
+     if (numbers[i] % 2 == 1)
+    {
+      
+      count += numbers[i];
+    } 
+     Console.Write(numbers[i] + ", ");
+     
 
-for(int i = 0; i < arr.Length; i++)
-{
-    Console.Write(arr[i] + " ");
-}
+ }
+   Console.WriteLine();
+   Console.Write("Сумма нечетных чисел массива равна: " + count);
