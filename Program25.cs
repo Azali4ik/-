f48aa7d6fb446fc8fa2c73eef2210113a823f071
Унугярн Даниел <oreol2005@gmail.com>
2022-08-24 21:18:07 +0300

    first commit1

--- Program25.cs
+++ Program25.cs
@@ -1,8 +1,17 @@
-﻿Console.Write("Введите число: ");
-int numA= Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int numB= Convert.ToInt32(Console.ReadLine());
+﻿int size = 10;
+int[] numbers = new int[size];
+int count = 0; 
+for (int i = 1; i < numbers.Length; i++)
+{
+  
+    numbers[i] = new Random().Next(99, 1000);
+     if (numbers[i] % 2 ==0)
+    {
+      count++;
+    } 
+     Console.Write(numbers[i] + ", ");
+     
 
-Math.Pow(numA, numB);
-   
-Console.WriteLine(Math.Pow(numA, numB));
+ }
+   Console.WriteLine();
+   Console.Write("количество четныйх чисел в данном массиве равно: " + count);
